ad7bbf90ac5d435fc33b23128e84d6142e19b405
jonathan lopez <jdavelg@gmail.com>
2020-07-14 20:34:15 -0600

    name app changed

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist/blog-angular'));
+app.use(express.static(__dirname+'/dist/blogangular'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/blog-angular/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/blogangular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
