43e5d94a254815c194b57f5291105e2707c12ea5
jonathan lopez <jdavelg@gmail.com>
2020-07-14 22:36:31 -0600

    new changes on server

--- src/app/server.js
+++ src/app/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist/blogangular'));
+app.use(express.static(__dirname+'/dist/blog-angular'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/blogangular/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/blog-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
